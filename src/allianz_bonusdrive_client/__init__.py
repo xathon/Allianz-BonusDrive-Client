859cc9feb655eaf95c877ca9ee15d08ac0e7a2cb
Alex Fuchs <github@xathon.de>
2025-10-22 05:58:31 +0200

    feat: update module init file to include data classes

--- src/allianz_bonusdrive_client/__init__.py
+++ src/allianz_bonusdrive_client/__init__.py
@@ -1,3 +1,18 @@
 from .client import BonusdriveAPIClient
+from .utils.dataclasses import (
+    Trip, EventData, Events, SnappedGeometry, Vehicle, User, TripScores, Scores, Badge, BadgeLevel
+)
 
-__all__ = ["BonusdriveAPIClient"]
+__all__ = [
+    "BonusdriveAPIClient",
+    "Trip",
+    "EventData",
+    "Events",
+    "SnappedGeometry",
+    "Vehicle",
+    "User",
+    "TripScores",
+    "Scores",
+    "Badge",
+    "BadgeLevel",
+]
